de1df783813dca2a72c554fd264cd665359219db
lennie <lennieli@hotmail.com>
2017-02-25 23:08:42 +0800

    事件函数封装，on、off、hover、toggle

--- LEN.js
+++ LEN.js
@@ -211,57 +211,6 @@
 	};
 
 
-
-//parseHTML
-LEN.fn.extend({
-	parseHTML:(function(){
-		var div = document.createElement('div');
-		function parseHTML (html){
-			var res = [];
-			div.innerHTML = html;
-			for (var i = 0; i < div.childNodes.length; i++) {
-				res.push(div.childNodes[i]);
-			}
-			div.innerHTML = '';
-			return res;
-		}
-		return parseHTML;
-	})(),
-	appendTo:function(dom){
-		var LENobj = this.constructor(dom);
-		var newObj = LEN(); //函数内的this仅指向单一对象，若需要为多个dom添加子元素，需要对this进行克隆，为满足链式编程，需改变return的内容，包含所有克隆出来的对象
-		this.each(function(v,i){
-			for(var j = 0 ; j < LENobj.length ; j++){ //若appendTo后面的LEN对象包含多个对象，则需要为每一个内部对象都添加对应的HTML
-				var temp =  j == LENobj.length-1 ? v : v.cloneNode(true);  //判断是否是循环的最后一次，若是最后一次则不用克隆，使用最开始的元素即可
-				push.call(newObj , temp);
-				LENobj[j].appendChild(temp);
-			}
-		});
-		return newObj;
-	},
-	append:function(selector){//LEN(".c").append("<div>div</div></p>p</p>")
-		this.constructor(selector).appendTo(this);
-		return this;
-	},
-	prependTo:function(dom){
-		var LENobj = this.constructor(dom);
-		var newObj = this.constructor(); //函数内的this仅指向单一对象，若需要为多个dom添加子元素，需要对this进行克隆，为满足链式编程，需改变return的内容，包含所有克隆出来的对象
-		this.each(function(v,i){
-			for(var j = 0 ; j < LENobj.length ; j++){ //若appendTo后面的LEN对象包含多个对象，则需要为每一个内部对象都添加对应的HTML
-				var temp =  j == LENobj.length-1 ? v : v.cloneNode(true);  //判断是否是循环的最后一次，若是最后一次则不用克隆，使用最开始的元素即可
-				push.call(newObj , temp);
-				LEN.fn.prependChild( LENobj[j],temp);
-			}
-		});
-		return newObj;
-	},
-	prepend:function(selector){
-		this.constructor(selector).prependTo(this);
-		return this ; 
-	}
-});
-
-
 //选择器
 var select = (function(){
 
@@ -299,9 +248,6 @@ var select = (function(){
 		}
 		return res ; 
 	};
-
-
-
 
 	/*//后代选择器1.0
 	var selectChild = function(selector , node){
@@ -377,47 +323,145 @@ var select = (function(){
 	// };
 
 	//并集选择器2.0
-	// var mixSelector = function (selector , node ){
-	// 	node = node || document; 
-	// 	var arr  = selector.split(',');
-	// 	var m = [];
-	// 	var res = [];
-	// 	forEach(arr,function(v,i){
-	// 		selectChild(v);
-	// 	});
-	// 	return res;
-	// };
-
-
-
 	var mixSelector = function (selector , node ){
 		node = node || document; 
-		var arr  = selector.split(',');
+		var arr  = selector.split(','); //分割
 		var str = '';
 		var res = [];
-		forEach(arr,function(v,i){
+		forEach(arr,function(v,i){  // 分别调用子代选择器
 			res = res.concat(selectChild(v));
 		});
 		return LEN.fn.unique(res); // 去除结果中的重复项
 	};
-
-
-
-
 	return mixSelector ;
-
-
-
-
-
 })();			
-LEN.select = select ;
+LEN.select = select ;  //扩展选项
 
 
 
 
+//DOM操作
+LEN.fn.extend({
+	parseHTML:(function(){
+		var div = document.createElement('div');
+		function parseHTML (html){
+			var res = [];
+			div.innerHTML = html;
+			for (var i = 0; i < div.childNodes.length; i++) {
+				res.push(div.childNodes[i]);
+			}
+			div.innerHTML = '';
+			return res;
+		}
+		return parseHTML;
+	})(),
+	appendTo:function(dom){
+		var LENobj = this.constructor(dom);
+		var newObj = LEN(); //函数内的this仅指向单一对象，若需要为多个dom添加子元素，需要对this进行克隆，为满足链式编程，需改变return的内容，包含所有克隆出来的对象
+		this.each(function(v,i){
+			for(var j = 0 ; j < LENobj.length ; j++){ //若appendTo后面的LEN对象包含多个对象，则需要为每一个内部对象都添加对应的HTML
+				var temp =  j == LENobj.length-1 ? v : v.cloneNode(true);  //判断是否是循环的最后一次，若是最后一次则不用克隆，使用最开始的元素即可
+				push.call(newObj , temp);
+				LENobj[j].appendChild(temp);
+			}
+		});
+		return newObj;
+	},
+	append:function(selector){//LEN(".c").append("<div>div</div></p>p</p>")
+		this.constructor(selector).appendTo(this);
+		return this;
+	},
+	prependTo:function(dom){
+		var LENobj = this.constructor(dom);
+		var newObj = this.constructor(); //函数内的this仅指向单一对象，若需要为多个dom添加子元素，需要对this进行克隆，为满足链式编程，需改变return的内容，包含所有克隆出来的对象
+		this.each(function(v,i){
+			for(var j = 0 ; j < LENobj.length ; j++){ //若appendTo后面的LEN对象包含多个对象，则需要为每一个内部对象都添加对应的HTML
+				var temp =  j == LENobj.length-1 ? v : v.cloneNode(true);  //判断是否是循环的最后一次，若是最后一次则不用克隆，使用最开始的元素即可
+				push.call(newObj , temp);
+				LEN.fn.prependChild( LENobj[j],temp);
+			}
+		});
+		return newObj;
+	},
+	prepend:function(selector){
+		this.constructor(selector).prependTo(this);
+		return this ; 
+	}
+});
 
 
+	//事件
+
+
+	
+	//事件  1.0
+	// LEN.fn.extend({
+	// 	click:function(fn){
+	// 		this.each(function(v,i){
+	// 			v.addEventListener('click',function(e){
+	// 				fn(e);
+	// 			}) 
+	// 		});
+	// 	}
+	// });
+	// var div = LEN('#dv');
+	// div.click(function(e){
+	// 	console.log(e);
+	// });
+
+	//事件  2.0
+
+	// LEN.fn.extend({
+	// 	click:function(fn){
+	// 		var oldEvent = function (){};
+	// 		this.each(function(v,i){
+	// 			if(v.onclick){
+	// 				oldEvent = v.onclick;
+	// 			}
+	// 			v.onclick = function (e){
+	// 				oldEvent(e);
+	// 				fn(e);
+	// 			};
+	// 		});
+	// 	}
+	// });
+
+	//on 3.0  //如何移除特定事件的指定函数？？？
+	LEN.fn.extend({
+		on:function( type , fn ){
+			var oldEvent = function (){};
+			this.each(function(v,i){
+				if(v['on'+type]){
+					oldEvent = v['on'+type];
+				}
+				v['on'+type] = function (e){
+					oldEvent(e);
+					fn(e);
+				};
+			});
+		},
+		off:function(type){
+			this.each(function(v,i){
+				if(v['on'+type]){
+					v['on'+type] = null;
+				}
+			});
+		},
+		hover:function(fn1,fn2){
+			this.on("mouseover" , fn1);
+			this.on("mouseout", fn2);
+		},
+
+		toggle:function(){   //轮流执行，传入的各函数
+			var arg = arguments ; 
+			var i = 0 ;
+			this.on('click' , function(){
+				arg[i]();
+				i++
+				i = i % arg.length ;
+			});
+		}
+	});
 
 
 
